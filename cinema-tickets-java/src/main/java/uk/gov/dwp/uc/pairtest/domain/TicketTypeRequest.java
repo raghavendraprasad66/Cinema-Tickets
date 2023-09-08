c17d386db4a52d80a4ebddde2a80ee46cc5e0cea
raghavendraprasad66 <raghavendra.araveti@gmail.com>
2023-09-08 22:19:48 +0100

    Updated TicketTypeRequest to immutable object.

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/domain/TicketTypeRequest.java
@@ -2,12 +2,14 @@ package uk.gov.dwp.uc.pairtest.domain;
 
 /**
  * Immutable Object
+ * 
+ * @author raghavendra.araveti
  */
 
-public class TicketTypeRequest {
+public final class TicketTypeRequest {
 
-    private int noOfTickets;
-    private Type type;
+    private final int noOfTickets;
+    private final Type type;
 
     public TicketTypeRequest(Type type, int noOfTickets) {
         this.type = type;
